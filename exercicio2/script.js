47e6ff36eeff903224ac5dd51215a22a701aed3f
nosdliw <108556065+nosdliw@users.noreply.github.com>
2022-07-20 13:50:54 -0300

    Finalizado atividade2

---
+++ exercicio2/script.js
@@ -0,0 +1,15 @@
+const prgnt1 = prompt("Você é aluno da Labenu?")
+const prgnt2 = prompt("Está gostando da forma de ensino?")
+const prgnt3 = prompt("Eles são chatos?")
+
+const rspst1 = (prgnt1)
+const rspst2 = (prgnt2)
+const rspst3 = (prgnt3)
+
+console.log("Você é aluno da Labenu? -", rspst1)
+console.log("Está gostando da forma de ensino? -", rspst2)
+console.log("Eles são chatos? -", rspst3)
+
+
+// const perguntaA = "Você está usando uma roupa azul hoje?";
+// const respostaA = prompt(perguntaA)
