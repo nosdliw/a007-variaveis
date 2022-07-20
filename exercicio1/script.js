a2c5f67fe0fdbfa9852e8d423e04e2c655e63e26
nosdliw <108556065+nosdliw@users.noreply.github.com>
2022-07-20 13:14:35 -0300

    Finalizada atividade 1

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+let nome = prompt("Qual o seu nome?")
+let idade = prompt("qual a sua idade?")
+
+console.log(typeof nome) //printou unedefind porque não atribui nenhum valor as variáveis
+console.log(typeof idade) // aconteceu a mesma coisa com a variável idade
+
+console.log("Olá", nome, "você tem", idade, " anos")
